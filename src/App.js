7a516d02d0e6296dafe5b6fe5429f7b7d067b75a
Charmaine Connors <charmainez.83@googlemail.com>
2020-05-25 13:57:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './components/Person';
 
-function App() {
-  return (
+class App extends Component {
+
+  state = {
+     /*persons: [
+      {name: "charmaine", age: 36},
+      {name: "micah", age: 10},
+      {name: "roser", age: 37},
+      {name: "eileen", age: 56}
+    ]*/
+    
+   numbers: [1, 2, 3, 4, 5, 6, 7],
+   currentNumber: ""
+
+  }
+
+  saveNumberHandler = (event) => {
+    console.log(event.target.value);
+    this.setState({
+      currentNumber: event.target.value
+    })
+  }
+
+  addNumberHandler = () => {
+    this.setState({
+      numbers: [...this.state.numbers, this.state.currentNumber],
+      currentNumber: ""
+    })
+  }
+
+  render () {
+    /*const individualPeople = this.state.persons.map ((person, index) => {
+      return <Person name={person.name} age={person.age} key={index}/>
+    }) */
+
+    const eachNumber = this.state.numbers.map((number, index) => {
+    return <li key={index}>{number}</li>
+    })
+
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <ul>
+        {eachNumber}
+      </ul>
+      <input type="number" onChange={this.saveNumberHandler} value={this.state.currentNumber} />
+      <button onClick={this.addNumberHandler}>Add numbers with this button</button>
+          
+       </div> 
+
+    )
+
+  }
+
 }
+
 
 export default App;
